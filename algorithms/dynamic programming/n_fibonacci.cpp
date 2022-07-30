d7898cfca8f0908b5354bff06bf92c018ec03f11
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-07-30 17:22:08 +0530

    n-fibonacci algorithm added using dp

---
+++ algorithms/dynamic programming/n_fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+long long int fibo(int n, std::map<int, long long int> &memo)
+{
+    if (memo[n])
+        return memo[n];
+    if (n <= 2)
+        return 1;
+    memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
+    std::cout << memo[n] << " ";
+    return memo[n];
+}
+
+int main()
+{
+    std::map<int, long long int> memo;
+    std::cout << fibo(50, memo);
+    return 0;
+}
