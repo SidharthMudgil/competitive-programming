d3c063e4712f4d6a369b8353e3306d0a8aa31d5a
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-09 16:44:23 +0530

    leetcode problem 26 added

---
+++ LeetCode/26.kt
@@ -0,0 +1,27 @@
+// USER: SIDHARTH MUDGIL
+// DATE: Saturday 09, Sep 2023
+// TASK: 26. Remove Duplicates from Sorted Array
+
+// Slower one
+// class Solution {
+//     fun removeDuplicates(nums: IntArray): Int {
+//         val map = nums.groupBy{it}.mapValues{it.value.size}
+//         var k = 0
+//         map.forEach{ it, `val` -> 
+//             nums[k++] = it
+//         }
+//         return k
+//     }
+// }
+
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        var k = 1
+        for (i in 1 ..< nums.size) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i]
+            }
+        }
+        return k
+    }
+}
