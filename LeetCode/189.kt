dd4465cc8d16b3c463d5629c68e2364a2ab253fb
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-11 17:10:17 +0530

    leetcode problem 189 added

---
+++ LeetCode/189.kt
@@ -0,0 +1,15 @@
+// USER: SIDHARTH MUDGIL
+// DATE: Monday 11, Sep 2023
+// TASK: 189. Rotate Array
+
+// cyclic replacement
+
+
+// Reversing Array
+// class Solution {
+//     fun rotate(nums: IntArray, k: Int): Unit {
+//         nums.reverse(0, nums.size - k % nums.size)
+//         nums.reverse(nums.size - k % nums.size, nums.size)
+//         nums.reverse()
+//     }
+// }
