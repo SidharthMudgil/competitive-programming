142089e0c7faf4a767c0f83aad9de79207617f4c
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-09-29 14:08:34 +0530

    redundant code removed

--- hackerrank/between-two-sets.kt
+++ hackerrank/between-two-sets.kt
@@ -2,33 +2,6 @@
 // DATE: Friday 29, Sep 2023
 // TASK: Between Two Sets
 
-// gcd
-fun _gcd(a: Int, b: Int): Int {
-    if (b == 0)
-        return a
-        
-    _gcd(b, a % b)
-}
-
-fun _lcm(a: Int, b: Int): Int {
-    return return (a * b) / _gcd(a, b)
-}
-
-fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
-    var lcm = a.reduce{ acc, i -> _lcm(acc, i) }
-    val gcd = b.reduce{ acc, i -> _gcd(acc, i) }
-    var res = 0
-    
-    for (i in lcmA..gcdB step lcmA) {
-        if (gcdB % i == 0) {
-            count++
-        }
-    }
-
-    return res
-}
-
-// loops
 fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
     val low = a.max()
     val high = b.min()
