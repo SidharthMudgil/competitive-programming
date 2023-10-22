6051e4c011c14ad735260197066006b06e167fd9
Sidharth <68889544+SidharthMudgil@users.noreply.github.com>
2023-10-22 14:14:16 +0530

    new hackerrank problem sherlock-and-array added

---
+++ hackerrank/sherlock-and-array.kt
@@ -0,0 +1,22 @@
+// USER: SIDHARTH MUDGIL
+// DATE: Sunday 22, Oct 2023
+// PROBLEM: Sherlock and Array
+
+fun balancedSums(arr: Array<Int>): String {
+    val pSum = mutableMapOf<Int, Int>()
+    val sSum = mutableMapOf<Int, Int>()
+    
+    pSum[0] = 0
+    sSum[arr.size - 1] = 0
+    
+    for (i in 1 ..< arr.size) {
+        pSum[i] = pSum[i - 1]!! + arr[i - 1]
+        sSum[arr.size - 1 - i] = sSum[arr.size - i]!! + arr[arr.size - i]
+    }
+    
+    for (i in 0 ..< arr.size) {
+        if (pSum[i]!! == sSum[i]!!) return "YES"
+    }
+
+    return "NO"
+}   
