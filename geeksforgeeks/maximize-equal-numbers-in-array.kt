d00601fdc1eea1275373964c42beb560fd939fb3
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-12-09 22:22:57 +0530

    add optimized solution

--- geeksforgeeks/maximize-equal-numbers-in-array.kt
+++ geeksforgeeks/maximize-equal-numbers-in-array.kt
@@ -15,3 +15,26 @@ fun maximizeEqualNumbers(arr: IntArray, k: Int): Int {
     
     return res
 }
+
+// optimized using prefix
+Int maximizeEqualNumbers(long[] arr, int k) {
+    long[] map = new long[200005];
+    i = 1;
+    while (i <= n) {
+        long l = a[(int)i] - k;
+        long r = a[(int)i] + k;
+        map[(int)l] = map[(int)l] + 1;
+        map[(int)(r + 1)] = map[(int)(r + 1)] - 1;
+        i = i + 1;
+    }
+    
+    long res = 1;
+    i = 1;
+    while (i <= 200000) {
+        map[(int)i] = map[(int)(i - 1)] + map[(int)i];
+        answer = Math.max(map[(int)i], answer);
+        i = i + 1;
+    }
+    
+    return res;
+}
